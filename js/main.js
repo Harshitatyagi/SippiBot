73067dbb98ab6d7032427b1019514e6522c182a9
Harshitatyagi <100477854+Harshitatyagi@users.noreply.github.com>
2023-01-27 20:46:30 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,89 @@
+let mic = document.getElementById("mic");
+let chatareamain = document.querySelector('.chatarea-main');
+let chatareaouter = document.querySelector('.chatarea-outer');
+
+let hi=["Hii!"];
+let hello=["Hello"];
+let intro = ["Hello, I am Sippi", "Hi, I am a SippiBot", "Hello, My name is Sippi"];
+let help = ["How may i assist you?","How can i help you?","What i can do for you?"];
+let greetings = ["i am good you little piece of love", "i am fine, what about you", "don't want to talk", "i am good"];
+let hobbies = ["i love to talk with humans", "i like to make friends like you", "i like cooking"];
+let pizzas = ["which type of pizza do you like?", "i can make a pizza for you", "i would love to make a pizza for you", "would you like cheese pizza?"];
+let thank = ["Most welcome","Not an issue","Its my pleasure","Mention not"];
+let closing = ['Ok bye-bye','As you wish, bye take-care','Bye-bye, see you soon..']
+
+const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+const recognition = new SpeechRecognition();
+
+function showusermsg(usermsg){
+    let output = '';
+    output += `<div class="chatarea-inner user">${usermsg}</div>`;
+    chatareaouter.innerHTML += output;
+    return chatareaouter;
+}
+
+function showchatbotmsg(chatbotmsg){
+    let output = '';
+    output += `<div class="chatarea-inner chatbot">${chatbotmsg}</div>`;
+    chatareaouter.innerHTML += output;
+    return chatareaouter;
+}
+
+function chatbotvoice(message){
+    const speech = new SpeechSynthesisUtterance();
+    speech.text = "Cannot recognize your Voice";
+    if(message.includes('who are you')){
+        let finalresult = intro[Math.floor(Math.random() * intro.length)];
+        speech.text = finalresult;
+    }
+    if(message.includes('hi' )){
+        let finalresult = hi[Math.floor(Math.random() * hi.length)];
+        speech.text = finalresult;
+    }
+    if(message.includes('hello' )){
+        let finalresult = hello[Math.floor(Math.random() * hello.length)];
+        speech.text = finalresult;
+    }
+    if(message.includes('fine')){
+        let finalresult = help[Math.floor(Math.random() * help.length)];
+        speech.text = finalresult;
+    }
+    if(message.includes('how are you' || 'how are you doing today')){
+        let finalresult = greetings[Math.floor(Math.random() * greetings.length)];
+        speech.text = finalresult;
+    }
+    if(message.includes('tell me something about you' || 'tell me something about your hobbies')){
+        let finalresult = hobbies[Math.floor(Math.random() * hobbies.length)];
+        speech.text = finalresult;
+    }
+    if(message.includes('pizza')){
+        let finalresult = pizzas[Math.floor(Math.random() * pizzas.length)];
+        speech.text = finalresult;
+    }
+    if(message.includes('thank you' || 'thank you so much')){
+        let finalresult = thank[Math.floor(Math.random() * thank.length)];
+        speech.text = finalresult;
+    }
+    if(message.includes('talk to you' || 'talk')){
+        let finalresult = closing[Math.floor(Math.random() * closing.length)];
+        speech.text = finalresult;
+    }
+    window.speechSynthesis.speak(speech);
+    chatareamain.appendChild(showchatbotmsg(speech.text));
+}
+
+recognition.onresult=function(e){
+    let resultIndex = e.resultIndex;
+    let transcript = e.results[resultIndex][0].transcript;
+    chatareamain.appendChild(showusermsg(transcript));
+    chatbotvoice(transcript);
+    console.log(transcript);
+}
+recognition.onend=function(){
+    mic.style.background="#ff3b3b";
+}
+mic.addEventListener("click", function(){
+    mic.style.background='#39c81f';
+    recognition.start();
+    console.log("Activated");
+})
